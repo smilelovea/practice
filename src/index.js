dc2fe3147c1448776f5e1a577538ec809e2d69d8
smilelove <smilelove_a@naver.com>
2020-08-18 15:12:33 +0900

    convertJSX

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ import ReactDOM from 'react-dom';
 
 // Create a react component 
 const App = () => {
-    return <div>hi</div>;
+    return (
+    <div>
+        <label class="label" for="name">
+            Enter name:
+        </label>
+        <input id="name" type="text" />
+        <button style={{ backgroundColor:"blue", 
+                        color:"white"}}>Submit</button>
+    </div>
+    );
 };
-
-
 // Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector('#root'));
